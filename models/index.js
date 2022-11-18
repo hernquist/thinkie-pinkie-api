0cebf7108d0ee69a665d3425914095cc0187a167
David Hernquist <david.hernquist@turner.com>
2022-11-18 01:14:54 -0500

    cleanup smodel index.js

--- models/index.js
+++ models/index.js
@@ -4,8 +4,6 @@ const user = {
   name: String,
   email: String,
   password: String,
-  //   practices: [practice],
-  //   upNextModules: [String],
 };
 
 const solution = {
@@ -22,54 +20,10 @@ const thinkiePinkie = {
   dayOfTheYear: Number,
 };
 
-// const practice = {
-//   userId: String,
-//   completedOn: String,
-//   topic: String,
-//   engagement: String,
-//   level: Number,
-//   totalQuestion: Number,
-//   totalCorrect: Number,
-//   score: Number,
-//   assessmentType: String,
-// };
-
-// const contentType = mongoose.Schema({
-//   name: String,
-//   type: String,
-// });
-
-// const moduleType = {
-//   content: [contentType],
-//   numberOfTurns: Number,
-//   slug: String,
-// };
-
-// const content = {
-//   name: String,
-//   type: String,
-//   list: [String],
-// };
-
-// const contentMap = {
-//   topic: String,
-//   engagement: [String],
-//   level: [Number],
-//   assessment: [[String]],
-// };
-
 const User = mongoose.model("User", user);
 const ThinkiePinkie = mongoose.model("ThinkiePinkie", thinkiePinkie);
-// const Practice = mongoose.model("Practice", practice);
-// const Module = mongoose.model("Module", moduleType);
-// const Content = mongoose.model("Content", content);
-// const ContentMap = mongoose.model("Content-Map", contentMap);
 
 module.exports = {
   User,
   ThinkiePinkie,
-  //   Practice,
-  //   Module,
-  //   Content,
-  //   ContentMap,
 };
